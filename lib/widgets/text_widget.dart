6b9974fd89e648c4c268429e07abacd14b6876b7
amrutapatil <patilamruta1099@gmail.com>
2024-03-28 06:12:46 +0530

    file was missing while commit

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_app_bloc/filterbloc/filter_bloc.dart';
+import 'package:to_do_app_bloc/searchBloc/search_bloc.dart';
 import 'package:to_do_app_bloc/to_do_model.dart';
+import 'package:to_do_app_bloc/todoListBloc/to_do_list_bloc.dart';
 
 class TextWidget extends StatelessWidget {
   final TextEditingController searchController = TextEditingController();
@@ -13,14 +17,23 @@ class TextWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         TextField(
-          controller: searchController,
+          onSubmitted: (value) {
+            context.read<ToDoListBloc>().add(AddTodoEvent(description: value));
+          },
+          controller: addTextController,
           decoration: const InputDecoration(label: Text("Add new task")),
         ),
         const SizedBox(
           height: 20,
         ),
         TextFormField(
-          controller: addTextController,
+          onChanged: (value) {
+            context.read<SearchBloc>().add(SearchTextEvent(searchText: value));
+          },
+          // onFieldSubmitted: (value) {
+          //   context.read<SearchBloc>().add(SearchTextEvent(searchText: value));
+          // },
+          controller: searchController,
           decoration:
               const InputDecoration(label: Text("Search To do"), filled: true),
         ),
@@ -40,10 +53,20 @@ class TextWidget extends StatelessWidget {
 
 Widget filterButton(BuildContext context, Filter filterType) {
   return TextButton(
-      onPressed: () {},
-      child: Text(filterType == Filter.active
-          ? "ACTIVE"
-          : filterType == Filter.all
-              ? "ALL"
-              : "COMPLETED"));
+    onPressed: () {
+      context
+          .read<FilterBloc>()
+          .add(ChangeFilterEvent(newSelectedFilter: filterType));
+    },
+    child: Text(
+        filterType == Filter.active
+            ? "ACTIVE"
+            : filterType == Filter.all
+                ? "ALL"
+                : "COMPLETED",
+        style: TextStyle(
+            color: context.watch<FilterBloc>().state.filterType == filterType
+                ? Colors.purple
+                : Colors.grey)),
+  );
 }
